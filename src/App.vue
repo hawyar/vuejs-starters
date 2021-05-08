4010ec6645170953c6d3e9b8ccd66648638ac6ec
hawyar <42186608+hawyar@users.noreply.github.com>
2021-05-08 14:05:51 -0400

    add: eslint, last commit from old mb

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld';
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
+
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
 
 <style>
 #app {
